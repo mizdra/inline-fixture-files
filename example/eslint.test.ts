4fa47f2368cc8261bad78297f791452106d54cc3
mizdra <pp.mizdra@gmail.com>
2023-06-26 02:00:26 +0900

    improve README

--- example/eslint.test.ts
+++ example/eslint.test.ts
@@ -1,32 +1,11 @@
-# @mizdra/inline-fs-fixtures
-
-The utility for writing fixture files inline.
-
-## Motivation
-
-When writing tests, it is often necessary to create fixture files. A common approach is to create a `fixture/` directory and write the fixture files there.
-
-```console
-$ ls -R fixture
-test-case-1/src:
-index.ts
-
-test-case-2/src:
-index.ts    math.ts
-```
-
-However, this approach leads to the test code and fixture file definitions being far apart. This makes it difficult to understand the test code.
-
-`@mizdra/inline-fs-fixtures` allows you to define fixture files in your test code. This makes the test code easier to understand.
-
-```typescript
 import { tmpdir } from 'node:os';
 import { join } from 'node:path';
 import dedent from 'dedent';
 import { ESLint } from 'eslint';
 import { expect, test } from 'vitest';
-import { createIFF } from '@mizdra/inline-fs-fixtures';
+import { createIFF } from '../src/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const fixtureDir = join(tmpdir(), 'inline-fs-fixtures', process.env['VITEST_POOL_ID']!);
 
 test('eslint reports lint errors', async () => {
@@ -63,21 +42,3 @@ test('eslint reports lint errors', async () => {
     2 problems"
   `);
 });
-```
-
-## Installation
-
-```console
-$ npm i -D @mizdra/inline-fs-fixtures
-```
-
-## Features
-
-- Write fixture files inline
-- TypeScript support
-- Cross-platform support
-- Zero dependencies
-
-## Available APIs
-
-See [`src/index.ts`](./src/index.ts).
