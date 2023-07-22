7f6567f83fa30e6ad3d30247ae2af52ce043bc32
mizdra <pp.mizdra@gmail.com>
2023-07-22 13:01:55 +0900

    refactoring test codes

--- src/index.test.ts
+++ src/index.test.ts
@@ -171,22 +171,24 @@ describe('CreateIFFResult', () => {
       });
     });
   });
-  test('fork', async () => {
-    const baseRootDir = join(fixtureDir, 'base');
-    const forkedRootDir = join(fixtureDir, 'forked');
-    const baseIff = await createIFF({ 'a.txt': 'a', 'b': { 'a.txt': 'b-a' } }, { rootDir: baseRootDir });
+  describe('fork', () => {
+    test('fork IFF', async () => {
+      const baseRootDir = join(fixtureDir, 'base');
+      const forkedRootDir = join(fixtureDir, 'forked');
+      const baseIff = await createIFF({ 'a.txt': 'a', 'b': { 'a.txt': 'b-a' } }, { rootDir: baseRootDir });
 
-    await baseIff.fork({ 'b': { 'b.txt': 'b-b' }, 'c.txt': 'c' }, { rootDir: forkedRootDir });
+      await baseIff.fork({ 'b': { 'b.txt': 'b-b' }, 'c.txt': 'c' }, { rootDir: forkedRootDir });
 
-    // `forkedIff` inherits fixtures from `baseIff`.
-    expect(await readFile(join(forkedRootDir, 'a.txt'), 'utf-8')).toMatchInlineSnapshot('"a"');
-    expect(await readFile(join(forkedRootDir, 'b/a.txt'), 'utf-8')).toMatchInlineSnapshot('"b-a"');
-    expect(await readFile(join(forkedRootDir, 'b/b.txt'), 'utf-8')).toMatchInlineSnapshot('"b-b"');
-    expect(await readFile(join(forkedRootDir, 'c.txt'), 'utf-8')).toMatchInlineSnapshot('"c"');
+      // `forkedIff` inherits fixtures from `baseIff`.
+      expect(await readFile(join(forkedRootDir, 'a.txt'), 'utf-8')).toMatchInlineSnapshot('"a"');
+      expect(await readFile(join(forkedRootDir, 'b/a.txt'), 'utf-8')).toMatchInlineSnapshot('"b-a"');
+      expect(await readFile(join(forkedRootDir, 'b/b.txt'), 'utf-8')).toMatchInlineSnapshot('"b-b"');
+      expect(await readFile(join(forkedRootDir, 'c.txt'), 'utf-8')).toMatchInlineSnapshot('"c"');
 
-    // The `baseIff` fixtures are left in place.
-    expect(await readFile(join(baseRootDir, 'a.txt'), 'utf-8')).toMatchInlineSnapshot('"a"');
-    expect(await readFile(join(baseRootDir, 'b/a.txt'), 'utf-8')).toMatchInlineSnapshot('"b-a"');
+      // The `baseIff` fixtures are left in place.
+      expect(await readFile(join(baseRootDir, 'a.txt'), 'utf-8')).toMatchInlineSnapshot('"a"');
+      expect(await readFile(join(baseRootDir, 'b/a.txt'), 'utf-8')).toMatchInlineSnapshot('"b-a"');
+    });
   });
 });
 
